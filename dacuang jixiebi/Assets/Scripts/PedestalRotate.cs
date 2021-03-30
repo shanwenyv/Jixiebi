05986715df05ccd1c809530f3d9ba74bb3ca79c6
shanwenyv1 <1357184723@qq.com>
2021-03-30 20:35:36 +0800

    模型主从关系
    第5次上传

---
+++ dacuang jixiebi/Assets/Scripts/PedestalRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedestalRotate : MonoBehaviour
+{
+    public Space m_RotateSpace;
+    public float m_RotateSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.left * m_RotateSpeed * Time.deltaTime, m_RotateSpace);
+    }
+}
