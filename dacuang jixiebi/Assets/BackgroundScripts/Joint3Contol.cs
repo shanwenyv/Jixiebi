f29aa65c358c11e19452e61f4ba651f7f5cb999c
shanwenyv1 <1357184723@qq.com>
2021-04-08 20:40:46 +0800

    关节1,2,4,5校准完成
    关节校准

--- dacuang jixiebi/Assets/BackgroundScripts/Joint3Contol.cs
+++ dacuang jixiebi/Assets/BackgroundScripts/Joint3Contol.cs
@@ -1,6 +1,7 @@
 ﻿
 /*****************************************************
-2021.4.7更新日志：完成了110°-180°的校准
+2021.4.7更新日志：完成了110°~180°的校准
+2021.4.8更新日志：完成了-180°~100°的校准
 *****************************************************/
 using System.Collections;
 using System.Collections.Generic;
@@ -9,16 +10,19 @@ using UnityEngine;
 public class Joint3Contol : MonoBehaviour
 {
     public float joint3Angle = 90;//关节3旋转角度
-    // Start is called before the first frame update
-    public Joint1Contol other1;
+   
+    //定义关节旋转角度
+    public float j3RotationSpeedX = 30;
+    public float j3RotationSpeedY = 0;
+    public float j3RotationSpeedZ = 0;
 
     void Start()
     {
-        
+
         //joint3Angle = joint3Angle - 5 ;
-        if(joint3Angle >= -90 && joint3Angle < 110)
+        /*if(joint3Angle >= -180 && joint3Angle < 110)
         {
-            //joint3Angle = joint3Angle + 70;
+            joint3Angle = joint3Angle - 90;
         }
         else if(joint3Angle >= 110 && joint3Angle <= 180)
         {
@@ -26,20 +30,38 @@ public class Joint3Contol : MonoBehaviour
         }
         else if(joint3Angle > 180 && joint3Angle <= 290)
         {
-            //joint3Angle = joint3Angle + 170;
+            joint3Angle = joint3Angle + 270;
         }
         else if(joint3Angle > 290 && joint3Angle <=360)
         {
             joint3Angle = joint3Angle - 290;
-        }
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        /*if (Input.GetKey(KeyCode.Keypad3) || Input.GetKey(KeyCode.Return))
+        {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(joint3Angle, 0, 0), 0.01f);///调用Joint2Contl中的旋转角度抵消臂1对臂2坐标的影响
+        }*/
         if (Input.GetKey(KeyCode.Keypad3) || Input.GetKey(KeyCode.Return))
         {
-                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(joint3Angle, other1.joint1Angle, 0), 0.01f);///调用Joint2Contl中的旋转角度抵消臂1对臂2坐标的影响
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 90), 0.01f);
+            if (joint3Angle >= 0 && joint3Angle <= 90)//到了指定角度就停止旋转
+            {
+                if (this.transform.localEulerAngles.x >= joint3Angle -0.5)
+                    j3RotationSpeedX = 0;
+                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
+                print("旋转了：" + this.transform.localEulerAngles.x);
+            }
+            else if (joint3Angle > 90 && joint3Angle <= 180)//到了指定角度就停止旋转
+            {
+                if (this.transform.localEulerAngles.x >= joint3Angle - (joint3Angle - 90))
+                    j3RotationSpeedX = 0;
+                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
+                print("旋转了：" + this.transform.localEulerAngles.x);
+            }
         }
     }
 }
--- dacuang jixiebi/Assets/BackgroundScripts/Joint5Contol.cs
+++ dacuang jixiebi/Assets/BackgroundScripts/Joint5Contol.cs
@@ -8,18 +8,36 @@ public class Joint5Contol : MonoBehaviour
     public float joint5Angle = 90;//关节5旋转角度
     public Joint1Contol other5;
 
+    public float j5RotationSpeedX = 0;
+    public float j5RotationSpeedY = 0;
+    public float j5RotationSpeedZ = 30;
+
     // Start is called before the first frame update
     void Start()
     {
-        joint5Angle = joint5Angle - 90;
+        //joint5Angle = joint5Angle - 90;
+        if (joint5Angle <= 0)
+        {
+            j5RotationSpeedZ = -j5RotationSpeedZ;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.Keypad5) || Input.GetKey(KeyCode.Return))
         {
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, joint5Angle, 0), 0.01f);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 90), 0.01f);
+            if(joint5Angle >= 0)
+            {
+                if (this.transform.localEulerAngles.z >= joint5Angle)
+                    j5RotationSpeedZ = 0;
+                transform.Rotate(new Vector3(j5RotationSpeedX, j5RotationSpeedY, j5RotationSpeedZ) * Time.deltaTime);
+            }
+            
+            //print("旋转" + this.transform.localEulerAngles.z);
+
         }
     }
 }
