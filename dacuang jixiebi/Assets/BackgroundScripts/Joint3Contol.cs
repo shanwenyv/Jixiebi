9cac5dcd02a2d87cd20da221189d2d8b83b5ed11
shanwenyv1 <1357184723@qq.com>
2021-04-09 20:36:04 +0800

    五轴全部校准完成
    关节校准完成

--- dacuang jixiebi/Assets/BackgroundScripts/Joint3Contol.cs
+++ dacuang jixiebi/Assets/BackgroundScripts/Joint3Contol.cs
@@ -15,10 +15,12 @@ public class Joint3Contol : MonoBehaviour
     public float j3RotationSpeedX = 30;
     public float j3RotationSpeedY = 0;
     public float j3RotationSpeedZ = 0;
+    private int t = 0;
+    private int a = 0;
 
     void Start()
     {
-
+        
         //joint3Angle = joint3Angle - 5 ;
         /*if(joint3Angle >= -180 && joint3Angle < 110)
         {
@@ -50,18 +52,66 @@ public class Joint3Contol : MonoBehaviour
             //transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 90), 0.01f);
             if (joint3Angle >= 0 && joint3Angle <= 90)//到了指定角度就停止旋转
             {
-                if (this.transform.localEulerAngles.x >= joint3Angle -0.5)
+                
+                if (this.transform.localEulerAngles.x >= joint3Angle -0.3)
                     j3RotationSpeedX = 0;
                 transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
                 print("旋转了：" + this.transform.localEulerAngles.x);
             }
-            else if (joint3Angle > 90 && joint3Angle <= 180)//到了指定角度就停止旋转
+            else if (joint3Angle > 90 && joint3Angle <= 180)
+            {
+                //print(90 - (joint3Angle - 90));
+                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
+                print("旋转了：" + this.transform.localEulerAngles.x);;
+                if (this.transform.localEulerAngles.x > 89.8)//校准：当旋转角度大于90的时候，启动检测判定（因为此模型转轴度数的特殊性，特写此算法）
+                {
+                    t = 1;
+                }
+                    
+                if(this.transform.localEulerAngles.x < 90 - (joint3Angle - 90.3) && t == 1)
+                {
+                     print("执行了233");
+                     j3RotationSpeedX = 0;
+                }            
+            }
+            else if(joint3Angle > 180 && joint3Angle <= 270)
+            {
+                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
+                print("旋转了：" + this.transform.localEulerAngles.x); ;
+                if (this.transform.localEulerAngles.x > 179.8)//校准：当旋转角度大于180的时候，启动检测判定（因为此模型转轴度数的特殊性，特写此算法）
+                {
+                    t = 1;
+                }
+                if (this.transform.localEulerAngles.x < 360.3 - (180 - (360 - joint3Angle)) && t == 1)
+                {
+                    print("执行了233");
+                    j3RotationSpeedX = 0;
+                }
+            }
+            else if(joint3Angle > 270 && joint3Angle <= 360)
+            {
+
+                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
+                print("旋转了：" + this.transform.localEulerAngles.x);
+                if (this.transform.localEulerAngles.x > 350.8)//校准：当旋转角度大于270的时候，启动检测判定（因为此模型转轴度数的特殊性，特写此算法）
+                {
+                    t = 1;
+                }
+                if(this.transform.localEulerAngles.x < 270.3 && t == 1)
+                {
+                    a = 1;
+                }
+                if (this.transform.localEulerAngles.x >= joint3Angle - 0.3 && a == 1)
+                    j3RotationSpeedX = 0;
+                    
+            }
+            /*else if (joint3Angle > 90 && joint3Angle <= 180)//到了指定角度就停止旋转
             {
                 if (this.transform.localEulerAngles.x >= joint3Angle - (joint3Angle - 90))
                     j3RotationSpeedX = 0;
                 transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
                 print("旋转了：" + this.transform.localEulerAngles.x);
-            }
+            }*/
         }
     }
 }
