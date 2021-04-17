0aa6c95acefb701c1a3ac5ce84896031194e4216
shanwenyv1 <1357184723@qq.com>
2021-04-17 10:31:46 +0800

    关节bug修复完毕
    2021.4.17

--- dacuang jixiebi/Assets/BackgroundScripts/Joint3Contol.cs
+++ dacuang jixiebi/Assets/BackgroundScripts/Joint3Contol.cs
@@ -1,23 +1,46 @@
-﻿
-/*****************************************************
-2021.4.7更新日志：完成了110°~180°的校准
-2021.4.8更新日志：完成了-180°~100°的校准
-*****************************************************/
+﻿/*******************************
+ * 因为物体角度大于180度时，实际角度为 360+真实角度 ,所以在判定时需要进行人为的校准
+ *****************************/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
 
 public class Joint3Contol : MonoBehaviour
 {
-    public float joint3Angle = 90;//关节3旋转角度
-   
-    //定义关节旋转角度
-    public float j3RotationSpeedX = 30;
-    public float j3RotationSpeedY = 0;
-    public float j3RotationSpeedZ = 0;
     private int t = 0;
     private int a = 0;
-
+    public static Joint3Contol Instance3;
+    public float joint3Angle = 90;//关节2旋转角度
+    //关节旋转角度
+    public float j3RotationSpeedX = 30;//设定初始运动速度
+    public float j3RotationSpeedY = 0;
+    public float j3RotationSpeedZ = 0;
+    public float joint3AngleAbjust = 0;//在运动代码部分，当旋转角度大于180度时，校准度数
+    public float joint3SpeedAbjust = 0;//在重置速度部分，当旋转角度大于180度时，校准度数
+    private void Awake()
+    {
+        Instance3 = this;                            //单例模式
+    }
+    public float GetInspectorRotationValueMethod(Transform transform)
+    {
+        // 获取原生值
+        System.Type transformType = transform.GetType();
+        PropertyInfo m_propertyInfo_rotationOrder = transformType.GetProperty("rotationOrder", BindingFlags.Instance | BindingFlags.NonPublic);
+        object m_OldRotationOrder = m_propertyInfo_rotationOrder.GetValue(transform, null);
+        MethodInfo m_methodInfo_GetLocalEulerAngles = transformType.GetMethod("GetLocalEulerAngles", BindingFlags.Instance | BindingFlags.NonPublic);
+        object value = m_methodInfo_GetLocalEulerAngles.Invoke(transform, new object[] { m_OldRotationOrder });
+        string temp = value.ToString();
+        //将字符串第一个和最后一个去掉
+        temp = temp.Remove(0, 1);
+        temp = temp.Remove(temp.Length - 1, 1);
+        //用‘，’号分割
+        string[] tempVector3;
+        tempVector3 = temp.Split(',');
+        //将分割好的数据传给Vector3
+        Vector3 vector3 = new Vector3(float.Parse(tempVector3[0]), float.Parse(tempVector3[1]), float.Parse(tempVector3[2]));
+        return vector3.x;
+    }
     void Start()
     {
       
@@ -26,78 +49,80 @@ public class Joint3Contol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float currentRotateX = transform.eulerAngles.x;
+        if (currentRotateX > 180)
+        {
+            currentRotateX = currentRotateX - 360;//这样-5度还是-5度 而不是355！
+        }
         if (Input.GetKey(KeyCode.Keypad3) || Input.GetKey(KeyCode.Return))
         {
-            if (joint3Angle >= 0 && joint3Angle <= 90)//到了指定角度就停止旋转
+            if (GetInspectorRotationValueMethod(transform) < 0)//如果旋转角度大于180度，则矫正角度到真实角度
             {
-                if (this.transform.localEulerAngles.x >= joint3Angle -0.5)
-                    j3RotationSpeedX = 0;
-                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
-                //print("旋转了：" + this.transform.localEulerAngles.x);
+                joint3AngleAbjust = 360 + GetInspectorRotationValueMethod(transform);
             }
-            else if (joint3Angle > 90 && joint3Angle <= 180)
+            //print("旋转了：" + GetInspectorRotationValueMethod(transform));//输出旋转角度
+            if (joint3Angle >= 0)//当设定角度大于初始角度时
             {
-                //print(90 - (joint3Angle - 90));
-                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
-                //print("旋转了：" + this.transform.localEulerAngles.x);;
-                if (this.transform.localEulerAngles.x > 89.5)//校准：当旋转角度大于90的时候，启动检测判定（因为此模型转轴度数的特殊性，特写此算法）
+                //print("旋转了：" + this.transform.localEulerAngles.z);
+                if (j3RotationSpeedX > 0)
                 {
-                    t = 1;
-                }
-                    
-                if(this.transform.localEulerAngles.x < 90 - (joint3Angle - 90.5) && t == 1)
-                {
-                     //print("执行了233");
-                     j3RotationSpeedX = 0;
-                }            
-            }
-            else if(joint3Angle > 180 && joint3Angle <= 270)
-            {
-                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
-                //print("旋转了：" + this.transform.localEulerAngles.x); ;
-                if (this.transform.localEulerAngles.x > 179.8)//校准：当旋转角度大于180的时候，启动检测判定（因为此模型转轴度数的特殊性，特写此算法）
-                {
-                    t = 1;
-                }
-                if (this.transform.localEulerAngles.x < 360.5 - (180 - (360 - joint3Angle)) && t == 1)
-                {
-                    //print("执行了233");
-                    j3RotationSpeedX = 0;
-                }
-            }
-            else if(joint3Angle > 270 && joint3Angle <= 360)
-            {
+                    if (GetInspectorRotationValueMethod(transform) > 0)//角度在0到180之间时，正常旋转
+                    {
+                        if (GetInspectorRotationValueMethod(transform) >= joint3Angle + 0.5)
+                        {
+                            j3RotationSpeedX = 0;
 
+                        }
+                    }
+                    else if (GetInspectorRotationValueMethod(transform) < 0)//角度在180到360之间时，先通过角度校准，再进行旋转
+                    {
+                        if (joint3AngleAbjust >= joint3Angle + 0.5)
+                        {
+                            j3RotationSpeedX = 0;
+
+                        }
+                    }
+                }
+                else if (j3RotationSpeedX < 0)//当设定角度小于初始角度时
+                {
+                    if (GetInspectorRotationValueMethod(transform) > 0)//角度在0到180之间时，正常旋转
+                    {
+                        if (GetInspectorRotationValueMethod(transform) <= joint3Angle + 0.5)
+                        {
+                            j3RotationSpeedX = 0;
+                        }
+                    }
+                    else if (GetInspectorRotationValueMethod(transform) < 0)//角度在180到360之间时，先通过角度校准，再进行旋转
+                        if (joint3AngleAbjust <= joint3Angle + 0.5)
+                        {
+                            j3RotationSpeedX = 0;
+
+                        }
+                }
                 transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
-                //print("旋转了：" + this.transform.localEulerAngles.x);
-                if (this.transform.localEulerAngles.x > 350.5)//校准：当旋转角度大于270的时候，启动检测判定（因为此模型转轴度数的特殊性，特写此算法）
-                {
-                    t = 1;
-                }
-                if(this.transform.localEulerAngles.x < 270.5 && t == 1)
-                {
-                    a = 1;
-                }
-                if (this.transform.localEulerAngles.x >= joint3Angle - 0.5 && a == 1)
-                    j3RotationSpeedX = 0;
-                
 
             }
-            /*else if (joint3Angle > 90 && joint3Angle <= 180)//到了指定角度就停止旋转
-            {
-                if (this.transform.localEulerAngles.x >= joint3Angle - (joint3Angle - 90))
-                    j3RotationSpeedX = 0;
-                transform.Rotate(new Vector3(j3RotationSpeedX, j3RotationSpeedY, j3RotationSpeedZ) * Time.deltaTime);
-                print("旋转了：" + this.transform.localEulerAngles.x);
-             }*/
-            
         }
         if (Input.GetKey(KeyCode.KeypadEnter))
         {
-            j3RotationSpeedX = 30;
-            a = 0;
-            t = 0;
+            j3RotationSpeedX = 30;//按下小键盘回车，重置速度初始值
+            joint3SpeedAbjust = 0;//重置速度调整值
+            joint3AngleAbjust = 0;//重置角度调整值
+
+            if (GetInspectorRotationValueMethod(transform) < 0)//如果物体角度大于180度，则校准角度
+            {
+                t = 1;
+                joint3SpeedAbjust = 360 + GetInspectorRotationValueMethod(transform);
+
+            }
+            if (joint3Angle < GetInspectorRotationValueMethod(transform) - 0.5)//物体大于180度时，校准速度。
+            {
+                j3RotationSpeedX = -j3RotationSpeedX;
+            }
+            else if (joint3Angle < joint3SpeedAbjust)
+            {
+                j3RotationSpeedX = -j3RotationSpeedX;
+            }
         }
     }
 }
